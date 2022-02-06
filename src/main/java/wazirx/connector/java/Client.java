60c90a50d8454609a8469873a0b1297315ce93b1
Prateek Sharma <prateeksharma.2801@gmail.com>
2022-02-06 21:43:13 +0530

    Order APIs integration with parameters

--- src/main/java/wazirx/connector/java/Client.java
+++ src/main/java/wazirx/connector/java/Client.java
@@ -64,39 +64,104 @@ public class Client extends BaseClient {
 		return this.call("historical_trades", params);
 	}
 
+	private Map<String, Object> getCreateOrderData(String symbolName, String type, Double quantity, Double price, Double stopPrice) throws Exception {
+		if(type == null || !(type.equalsIgnoreCase("limit") || type.equalsIgnoreCase("stop_limit"))) {
+			throw new Exception("Invalid type provided");
+		}
+		Map<String, Object> params = Map.of(
+				"symbol", symbolName,
+				"side", "buy",
+				"type", type,
+				"recvWindow", 10000,
+				"timestamp", System.currentTimeMillis());
+		if(type.equalsIgnoreCase("limit")) {
+			if(quantity == null || price == null) {
+				throw new Exception("Both quantity and price should be non null");
+			}
+			params.put("quantity", quantity);
+			params.put("price", price);
+		} else if(type.equalsIgnoreCase("stop_limit")) {
+			if(quantity == null || price == null || stopPrice == null) {
+				throw new Exception("Quantity, price and stopPrice should be non null");
+			}
+			params.put("quantity", quantity);
+			params.put("price", price);
+			params.put("stopPrice", stopPrice);
+		}
+		return params;
+	}
+
 	//	create_order
-	public String createOrder() throws Exception {
-		return this.call("create_order", null);
+	public String createOrder(String symbolName, String type, Double quantity, Double price, Double stopPrice) throws Exception {
+		return this.call("create_order", this.getCreateOrderData(symbolName, type, quantity, price, stopPrice));
 	}
 
 	//	create_test_order
-	public String createTestOrder() throws Exception {
-		return this.call("create_test_order", null);
+	public String createTestOrder(String symbolName, String type, Double quantity, Double price, Double stopPrice) throws Exception {
+		return this.call("create_test_order", this.getCreateOrderData(symbolName, type, quantity, price, stopPrice));
 	}
 
 	//	query_order
-	public String queryOrder() throws Exception {
-		return this.call("query_order", null);
-	}
-
-	//	cancel_order
-	public String cancelOrder() throws Exception {
-		return this.call("cancel_order", null);
+	public String queryOrder(long orderId) throws Exception {
+		Map<String, Object> params = Map.of(
+				"orderId", orderId,
+				"recvWindow", 60000,
+				"timestamp", System.currentTimeMillis());
+		return this.call("query_order", params);
 	}
 
 	//	open_orders
-	public String openOrders() throws Exception {
-		return this.call("open_orders", null);
-	}
-
-	//	cancel_open_orders
-	public String cancelOpenOrders() throws Exception {
-		return this.call("cancel_open_orders", null);
+	public String openOrders(String symbolName, Long orderId) throws Exception {
+		Map<String, Object> params = Map.of(
+				"recvWindow", 60000,
+				"timestamp", System.currentTimeMillis());
+		if(symbolName != null && symbolName.length() > 0) {
+			params.put("symbol", symbolName);
+		}
+		if(orderId != null) {
+			params.put("orderId", orderId);
+		}
+		return this.call("open_orders", params);
 	}
 
 	//	all_orders
-	public String allOrders() throws Exception {
-		return this.call("all_orders", null);
+	public String allOrders(String symbolName, Long orderId, Long startTime, Long endTime, Integer limit) throws Exception {
+		Map<String, Object> params = Map.of(
+				"symbol", symbolName,
+				"recvWindow", 60000,
+				"timestamp", System.currentTimeMillis());
+		if(orderId != null) {
+			params.put("orderId", orderId);
+		}
+		if(startTime != null) {
+			params.put("startTime", startTime);
+		}
+		if(endTime != null) {
+			params.put("endTime", endTime);
+		}
+		if(limit != null) {
+			params.put("limit", limit);
+		}
+		return this.call("all_orders", params);
+	}
+
+	//	cancel_order
+	public String cancelOrder(String symbolName, long orderId) throws Exception {
+		Map<String, Object> params = Map.of(
+				"symbol", symbolName,
+				"orderId", orderId,
+				"recvWindow", 60000,
+				"timestamp", System.currentTimeMillis());
+		return this.call("cancel_order", params);
+	}
+
+	//	cancel_open_orders
+	public String cancelOpenOrders(String symbolName) throws Exception {
+		Map<String, Object> params = Map.of(
+				"symbol", symbolName,
+				"recvWindow", 60000,
+				"timestamp", System.currentTimeMillis());
+		return this.call("cancel_open_orders", params);
 	}
 
 	//	account_info
